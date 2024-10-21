e81c3fe058e281b78c70146c52fb76e8c63ef8e2
camiurenda <urendacamila@gmail.com>
2024-10-21 18:29:55 -0300

    Mejoro CORS para que funcione el despliegue

--- server/index.js
+++ server/index.js
@@ -27,9 +27,24 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5000',
+  'https://proyecto2front.vercel.app/',
+  'https://proyecto2server.vercel.app/',
+];
 
-// Enable CORS
-app.use(cors());
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
 // Enable the use of request body parsing middleware
 app.use(bodyParser.json());
